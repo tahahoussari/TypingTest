456ac5b671935b0923d80cd3bb3db95202589def
tahahoussari <tahahoussari@gmail.com>
2023-01-19 17:22:08 -0500

    Finished up js code

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
 
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
     const arrayValue = quoteDisplayElement.value.split('')
+    
     let correct = true
 
     arrayQuote.forEach((characterSpan, index) => {
@@ -43,6 +45,20 @@ async function renderNewQuote(){
 
     })
     quoteInputElement.value = null
+    startTimer()
+}
+
+let startTime
+function startTimer(){
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+        timer.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime(){
+    return Math.floor((new Date() - startTime)/ 1000)
 }
 
 renderNewQuote()
