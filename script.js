8621afa12b4b59364e8663e185c3eb7163c15301
tahahoussari <tahahoussari@gmail.com>
2023-01-19 17:11:37 -0500

    Added script.js
    -Started working on making sure the letters are correct and indicating any mistakes

---
+++ script.js
@@ -0,0 +1,48 @@
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteDisplayElement.value.split('')
+    let correct = true
+
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if(character == null){
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        }
+        else if (character == characterSpan.innerText){
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        }else{
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+
+    if(correct) renderNewQuote()
+})
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(response => response.json() )
+        .then(data => data.content)
+}
+
+async function renderNewQuote(){
+    const quote = await  getRandomQuote()
+    quoteDisplayElement.innerHTML = ''
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+
+    })
+    quoteInputElement.value = null
+}
+
+renderNewQuote()
